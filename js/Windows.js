10de14e34ffb122ef1aeaeaa7f36c1b5879962cb
SirFerr <ursusmartimus007@gmail.com>
2022-10-17 10:42:10 +0300

    bootstrap top

--- js/Windows.js
+++ js/Windows.js
@@ -1,25 +1,4 @@
 const {BrowserWindow, Menu, Dialog} =require("electron");
-const template =[
-    {
-        label: "Выбор шаблона отчета",
-        submenu: [
-            {
-                label: "For Department",
-            },
-            {
-                label: "For Company",
-                click: async()=>{
-                    createWindowForCompany()
-                },
-
-            },
-            {
-                role:"close",
-            },
-        ],
-    }
-]
-
 
 createWindowForDepartment = ()=> {
     const forDepartment = new BrowserWindow({
@@ -37,22 +16,4 @@ createWindowForDepartment = ()=> {
     forDepartment.loadFile('html/forDepartment.html')
 
     forDepartment.webContents.openDevTools();
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
-}
-createWindowForCompany= ()=>{
-    const forCompany = new BrowserWindow({
-        width: 600,
-        height: 800,
-        title: "Window For Company",
-        resizable: true,
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModules: true
-        }
-    });
-    forCompany.loadFile('html/forCompany.html')
-    forCompany.webContents.openDevTools()
-    const menu = Menu.buildFromTemplate(template)
-    Menu.setApplicationMenu(menu)
 }
